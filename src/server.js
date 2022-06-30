54485456fcd6c08b650035dcae3e1d8e7980238e
eduardo-nanguelu <107730987+eduardo-nanguelu@users.noreply.github.com>
2022-06-30 07:27:02 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require('./app')
-app.listen(5000, () => console.log("Servidor corriendo en el puerto : "+5000))
+app.listen(3000, () => console.log("Servidor corriendo en el puerto : "+3000))
 
